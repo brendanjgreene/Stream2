2d067b0729dd373eaf5c62f338045fa75e105298
Brendan Greene <brendanjgreene@gmail.com>
2017-01-23 20:45:26 +0000

    created grid

---
+++ 13classes/fractions.py
@@ -0,0 +1,13 @@
+class Fraction:
+    def __init__(self, numerator, denominator):
+        self.num = numerator
+        self.den = denominator
+
+    def __add__(self, other):
+        num = self.num * other.den + self.den * other.num
+        den = self.den * other.den
+        return Fraction(num, den)
+
+    def __repr__(self):
+        return '%s/%s' % (self.num, self.den)
+
