685d7a21eae574770d169a953ae70943c6a1fd79
Brendan Greene <brendanjgreene@gmail.com>
2017-01-24 20:57:48 +0000

    my first mysql database

---
+++ 16mysqlDatabaseCreation/mydb.sql
@@ -0,0 +1,21 @@
+CREATE database mydb;
+
+USE mydb;
+
+CREATE TABLE people (
+        id INTEGER AUTO_INCREMENT,
+        first_name VARCHAR(50) NOT NULL,
+        second_name VARCHAR(50) NOT NULL,
+        DOB DATE NOT NULL,
+        PRIMARY KEY (id)
+);
+
+CREATE TABLE orders (
+        id INTEGER AUTO_INCREMENT,
+        amount DECIMAL(18,2) NOT NULL,
+        person_id INT,
+        created_at datetime DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (id),
+        FOREIGN KEY (person_id) REFERENCES people(id),
+        CHECK(amount>0)
+);
