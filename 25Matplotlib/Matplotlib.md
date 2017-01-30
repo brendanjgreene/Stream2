7e879523085fe400d48182c14b6799ce43a34523
Brendan Greene <brendanjgreene@gmail.com>
2017-01-30 22:55:07 +0000

    Using Pandas complete

--- 25Matplotlib/Matplotlib.md
+++ 25Matplotlib/Matplotlib.md
@@ -6,3 +6,13 @@
 ##UBar Charts and Histograms
 ##Pie Charts
 ##Using Pandas
+
+#SUMMARY
+
+#In this lesson, you explored:
+
+##Creating a basic line plot
+##Legends, Titles, and Labels
+##Bar Charts and Histograms
+##Pie Charts
+##Using Pandas
--- 25Matplotlib/pandas_test.py
+++ 25Matplotlib/pandas_test.py
@@ -16,3 +16,30 @@ print df.head()
 df['High'].plot()
 plt.legend()
 plt.show()
+
+web_stats = {'Day':[1,2,3,4,5,6],
+             'Visitors':[43,34,65,56,29,76],
+             'Bounce Rate':[65,67,78,65,45,52]}
+
+df = pandas.DataFrame(web_stats)
+print "DF HEAD"
+print df.head()
+print "------------------"
+print "DF TAIL"
+print df.tail()
+print "------------------"
+print "DF TAIL LIMITED TO 2"
+print df.tail(2)
+print "MAKE THE DAY THE INDEX"
+
+df.set_index('Day', inplace=True)
+
+print df.Visitors
+
+df['Visitors'].plot()
+plt.show()
+
+df.plot()
+plt.show()
+
+print df[['Visitors','Bounce Rate']]
