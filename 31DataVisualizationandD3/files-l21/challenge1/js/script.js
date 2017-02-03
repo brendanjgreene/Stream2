aa03c54f012f5e886bc78c40c4a4f9436b2500e9
Brendan Greene <brendanjgreene@gmail.com>
2017-02-03 13:19:03 +0000

    remove smallBox class

--- 31DataVisualizationandD3/files-l21/challenge1/js/script.js
+++ 31DataVisualizationandD3/files-l21/challenge1/js/script.js
@@ -1,19 +1,21 @@
 var body = d3.select("body");
 
-body.append("p").text("I Love Data")
-body.append("p").text("I Love Data")
-body.append("p").text("I Love Data")
+body.append("p").text("I Love Data");
+body.append("p").text("I Love Data");
+body.append("p").text("I Love Data");
 
-var myDiv = d3.select("div")
+var myDiv = d3.select("div");
 
-myDiv.append("p").text("paragraph inside the div")
+myDiv.append("p").text("paragraph inside the div");
 
-myDiv.attr('style','background-color:#ddd')
+myDiv.attr('style','background-color:#ddd');
 
-myDiv.attr('style','border:solid 4px black')
+myDiv.attr('style','border:solid 4px black');
 
-myDiv.attr('style','background-color:#ddd; border:solid 4px black')
+myDiv.attr('style','background-color:#ddd; border:solid 4px black');
 
-myDiv.attr('style','background-color:#ddd; border:solid 4px black; text-align:center')
+myDiv.attr('style','background-color:#ddd; border:solid 4px black; text-align:center');
 
 myDiv.classed("smallBox", true);
+
+myDiv.classed("smallBox", false);
