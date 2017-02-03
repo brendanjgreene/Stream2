b1f8fc196fd29be45a33a19323931d5209aee5ce
Brendan Greene <brendanjgreene@gmail.com>
2017-02-03 13:01:00 +0000

    append paragraph inside the div

--- 31DataVisualizationandD3/files-l21/challenge1/js/script.js
+++ 31DataVisualizationandD3/files-l21/challenge1/js/script.js
@@ -3,3 +3,7 @@ var body = d3.select("body")
 body.append("p").text("I Love Data")
 body.append("p").text("I Love Data")
 body.append("p").text("I Love Data")
+
+var myDiv = d3.select("div")
+
+myDiv.append("p").text("paragraph inside the div")
